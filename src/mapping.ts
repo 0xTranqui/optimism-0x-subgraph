4fd37f3b904737bdde89cf850d7d7e8178951291
0xTranqui <thinkandchill11@gmail.com>
2022-05-06 04:28:49 -0300

    updating with code behind v4 sugbraph used in prod of marketplace

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,67 +1,104 @@
-import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  ZeroEx,
-  ERC721OrderCancelled,
-  ERC721OrderFilled,
-  ERC721OrderPreSigned,
   ERC1155OrderCancelled,
-  ERC1155OrderFilled,
-  ERC1155OrderPreSigned
-} from "../generated/ZeroEx/ZeroEx"
-import { ExampleEntity } from "../generated/schema"
+  ERC1155OrderPreSigned,
+  ERC721OrderCancelled,
+  ERC721OrderPreSigned,
+} from '../generated/ZeroEx/ZeroEx';
+import { Erc1155Order, Erc721Order, Erc1155Cancellation, Erc721Cancellation, Fee, Property } from '../generated/schema';
+import { Entity, store } from '@graphprotocol/graph-ts'
 
-export function handleERC721OrderCancelled(event: ERC721OrderCancelled): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+export function handleErc721OrderPreSigned(event: ERC721OrderPreSigned): void {
+  let order = new Erc721Order(event.params.maker.toHex() + '-' + event.params.nonce.toString());
+  order.direction = event.params.direction;
+  order.maker = event.params.maker;
+  order.taker = event.params.taker;
+  order.expiry = event.params.expiry;
+  order.nonce = event.params.nonce;
+  order.erc20Token = event.params.erc20Token;
+  order.erc20TokenAmount = event.params.erc20TokenAmount;
+  let feeEntities: string[] = [];
+  let fees = event.params.fees;
+  for (let i = 0; i < fees.length; i++) {
+      let feeStruct = fees[i];
+      let feeEntity = new Fee(order.id + '-' + i.toString());
+      feeEntity.recipient = feeStruct.recipient;
+      feeEntity.amount = feeStruct.amount;
+      feeEntity.feeData = feeStruct.feeData;
+      feeEntity.save();
+      feeEntities.push(feeEntity.id);
   }
+  order.fees = feeEntities;
+  order.erc721Token = event.params.erc721Token;
+  order.erc721TokenId = event.params.erc721TokenId;
+  let propertyEntities: string[] = [];
+  let properties = event.params.erc721TokenProperties;
+  for (let i = 0; i < properties.length; i++) {
+      let propertyStruct = properties[i];
+      let propertyEntity = new Property(order.id + '-' + i.toString());
+      propertyEntity.propertyValidator = propertyStruct.propertyValidator;
+      propertyEntity.propertyData = propertyStruct.propertyData;
+      propertyEntity.save();
+      propertyEntities.push(propertyEntity.id);
+  }
+  order.logIndex = event.logIndex;
+  order.timestamp = event.block.timestamp;
+  order.blockNumber = event.block.number
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.maker = event.params.maker
-  entity.nonce = event.params.nonce
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // None
+  order.save();
 }
 
-export function handleERC721OrderFilled(event: ERC721OrderFilled): void {}
+export function handleErc1155OrderPreSigned(event: ERC1155OrderPreSigned): void {
+  let order = new Erc1155Order(event.params.maker.toHex() + '-' + event.params.nonce.toString());
+  order.direction = event.params.direction;
+  order.maker = event.params.maker;
+  order.taker = event.params.taker;
+  order.expiry = event.params.expiry;
+  order.nonce = event.params.nonce;
+  order.erc20Token = event.params.erc20Token;
+  order.erc20TokenAmount = event.params.erc20TokenAmount;
+  let feeEntities: string[] = [];
+  let fees = event.params.fees;
+  for (let i = 0; i < fees.length; i++) {
+      let feeStruct = fees[i];
+      let feeEntity = new Fee(order.id + '-' + i.toString());
+      feeEntity.recipient = feeStruct.recipient;
+      feeEntity.amount = feeStruct.amount;
+      feeEntity.feeData = feeStruct.feeData;
+      feeEntity.save();
+      feeEntities.push(feeEntity.id);
+  }
+  order.fees = feeEntities;
+  order.erc1155Token = event.params.erc1155Token;
+  order.erc1155TokenId = event.params.erc1155TokenId;
+  let propertyEntities: string[] = [];
+  let properties = event.params.erc1155TokenProperties;
+  for (let i = 0; i < properties.length; i++) {
+      let propertyStruct = properties[i];
+      let propertyEntity = new Property(order.id + '-' + i.toString());
+      propertyEntity.propertyValidator = propertyStruct.propertyValidator;
+      propertyEntity.propertyData = propertyStruct.propertyData;
+      propertyEntity.save();
+      propertyEntities.push(propertyEntity.id);
+  }
+  order.erc1155TokenProperties = propertyEntities;
+  order.erc1155TokenAmount = event.params.erc1155TokenAmount;
 
-export function handleERC721OrderPreSigned(event: ERC721OrderPreSigned): void {}
+  order.save();
+}
 
-export function handleERC1155OrderCancelled(
-  event: ERC1155OrderCancelled
-): void {}
+export function handleErc721OrderCancelled(event: ERC721OrderCancelled): void {
+  let id = event.params.maker.toHex() + '-' + event.params.nonce.toString();
+  let order = Erc721Order.load(id);
+  if (order != null) {
+      store.remove('Erc721Order', id);
+  }
+}
 
-export function handleERC1155OrderFilled(event: ERC1155OrderFilled): void {}
-
-export function handleERC1155OrderPreSigned(
-  event: ERC1155OrderPreSigned
-): void {}
+export function handleErc1155OrderCancelled(event: ERC1155OrderCancelled): void {
+  let id = event.params.maker.toHex() + '-' + event.params.nonce.toString();
+  let order = Erc1155Order.load(id);
+  if (order != null) {
+      store.remove('Erc1155Order', id);
+  }
+}
